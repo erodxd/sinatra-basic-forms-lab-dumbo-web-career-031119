db586adf36c98d0d1110a649bd08748bc5236db3
erodxd <emrod718@gmail.com>
2019-04-04 10:27:32 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get '/' do
+        erb :index
+    end
+
+    #renders a form that can POST a puppy
+    get '/new' do
+        erb :create_puppy
+    end
+
+    #recieves a request from /new @ /puppy
+    #@puppy instance variable created, with params for name, breed, & age
+    post '/puppy' do
+        @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+
+        erb :display_puppy
+    end
+
 end
